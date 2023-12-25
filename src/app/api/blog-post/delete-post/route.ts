3e2a0df7e81b28c792295d26867f03ba377c0b67
aimahusnain <aimahusnain@gmail.com>
2023-12-26 01:03:26 +0500

    taga

--- src/app/api/blog-post/delete-post/route.ts
+++ src/app/api/blog-post/delete-post/route.ts
@@ -1,34 +1,36 @@
 import prisma from "@/database";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest) {
-  try {
-    const url = new URL(req.url);
-    const extractIdOfBlogItemToBeDeleted = url.searchParams.get("id");
 
-    const deletedBlogPost = await prisma.post.delete({
-      where: {
-        id: String(extractIdOfBlogItemToBeDeleted),
-      },
-    });
+export async function DELETE(req: NextRequest){
+    try {
+        const url = new URL(req.url);
+        const extractIdOfBlogItemToBeDeleted = url.searchParams.get("id");
 
-    if (deletedBlogPost) {
-      return NextResponse.json({
-        success: true,
-        message: "Blog deleted successfully",
-      });
-    } else {
-      return NextResponse.json({
-        success: false,
-        message: "Failed to delete the blog! Please try again",
-      });
+        const deletedBLogPost = await prisma.post.delete({
+            where : {
+                id : Number(extractIdOfBlogItemToBeDeleted)
+            }
+        })
+
+        if (deletedBLogPost) {
+            return NextResponse.json({
+              success: true,
+              message: "Blog deleted successfully",
+            });
+          } else {
+            return NextResponse.json({
+              success: false,
+              message: "Failed to delete the blog ! Please try again",
+            });
+          }
+
+    } catch (e) {
+        console.log(e);
+
+        return  NextResponse.json({
+            success : false,
+            message : 'Somthing went wrong ! Please try again'
+        })
     }
-  } catch (e) {
-    console.error(e);
-
-    return NextResponse.json({
-      success: false,
-      message: 'Something went wrong! Please try again',
-    });
-  }
 }
